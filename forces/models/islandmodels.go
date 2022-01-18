0fe434ca642d8bab5f094c8f2f793c4087298556
mvee18 <emvalenc@go.olemiss.edu>
2022-01-18 14:08:05 -0600

    added bias value calculation with tests

--- forces/models/islandmodels.go
+++ forces/models/islandmodels.go
@@ -1,30 +1,57 @@
 package models
 
+import (
+	"ga/forces/utils"
+	"math"
+)
+
 // Unfortunately, we have multiple chromosomes.
 // As a result, we have to modify the version presented by Tsutshi, 1995.
-func CalculateBias(p []Organism) {
-
+// This will give a value 0.5 <= B <= 1.0.
+func CalculateBias(p []Organism) float64 {
 	// The formula specifies we compare the same gene across all the organisms
 	// in the population first, not each organism.
 	n := len(p)
+	final := 0.0
+	dna := p[0].DNA
 
-	for _, v := range p {
-		for j, chromosome := range v.DNA {
-			// We need all of the chromosomes in this column.
-			lChr := len(chromosome)
-			for k := range chromosome {
-				// Iterate again across all of the organisms (rows).
-				for m := range p {
-					c := 0.0
-					biasValue(&c, p[m].DNA[j][k], n, lChr)
-				}
+	// Iterate over the chromosomes of the first organism. Since they're all
+	// the same size, it doesn't matter which one we use.
+	for j, chromosome := range dna {
+		// We need all of the chromosomes in this column.
+		summation := 0.0
+		lChr := len(chromosome)
 
+		for k := range chromosome {
+			// Iterate again across all of the organisms (rows).
+			for m := range p {
+				biasValue(&summation, utils.SelectDomain(j+2), p[m].DNA[j][k], n)
+				// biasValue(&summation, 3, p[m].DNA[j][k], n)
+				// fmt.Printf("the domain is %v\n", utils.SelectDomain(j+2))
+				// fmt.Printf("summation int: %v\n", summation)
 			}
+
+			biasSummation(&summation, n)
+
+			// fmt.Printf("summation: %v, n: %v, lChr: %v\n", summation, n, lChr)
 		}
+
+		final += summation / float64(n) / float64(lChr)
 	}
 
+	return final / float64(len(dna))
 }
 
-func biasValue(count *float64, gene float64, n, l int) {
+func biasValue(count *float64, ub, gene float64, n int) {
+	// fmt.Printf("gene: %v, ub: %v\n", gene, ub)
+	*count += ((gene + ub) / (2 * ub))
+}
 
+func biasSummation(intermediate *float64, n int) {
+	*intermediate -= float64(n) / 2
+
+	*intermediate = math.Abs(*intermediate)
+	*intermediate += float64(n) / 2
+
+	// fmt.Printf("int out: %v\n", *intermediate)
 }
--- forces/models/models_test.go
+++ forces/models/models_test.go
@@ -1,13 +1,10 @@
 package models
 
 import (
-	"fmt"
-	"os"
 	"testing"
-
-	"github.com/ntBre/chemutils/summarize"
 )
 
+/*
 func TestSummarize(t *testing.T) {
 	f, err := os.Open("spectro.out")
 	if err != nil {
@@ -18,6 +15,7 @@ func TestSummarize(t *testing.T) {
 
 	fmt.Printf("%#v", result)
 }
+*/
 
 func BenchmarkCalcFitness(t *testing.B) {
 	t.Run("how slow is calc fitness?", func(b *testing.B) {
@@ -32,4 +30,132 @@ func BenchmarkCalcFitness(t *testing.B) {
 		}
 
 	})
+}
+
+func TestCalculateBias(t *testing.T) {
+
+	t.Run("testing with single chromosome population", func(t *testing.T) {
+		var d = DNA{
+			Chromosome{1.0},
+			// Chromosome{4.0, 5.0},
+			// Chromosome{7.0, 8.0},
+		}
+
+		var d2 = DNA{
+			Chromosome{1.0},
+			// Chromosome{4.0, -5.0},
+			// Chromosome{-7.0, 8.0},
+		}
+
+		var dopp = DNA{
+			Chromosome{-1.0},
+			// Chromosome{4.0, -5.0},
+			// Chromosome{-7.0, 8.0},
+		}
+
+		var o = Organism{
+			DNA:     d,
+			Path:    "",
+			Fitness: 0.0,
+		}
+
+		var o2 = Organism{
+			DNA:     d2,
+			Path:    "",
+			Fitness: 0.0,
+		}
+
+		var oopp = Organism{
+			DNA:     dopp,
+			Path:    "",
+			Fitness: 0.0,
+		}
+
+		t.Run("test with exactly the same", func(t *testing.T) {
+			pop := []Organism{o, o2}
+
+			bias := CalculateBias(pop)
+
+			want := 1.0
+
+			if bias != want {
+				t.Errorf("wrong bias, wanted %v, got %v\n", want, bias)
+			}
+		})
+
+		t.Run("test with exact opposites", func(t *testing.T) {
+			pop := []Organism{o, oopp}
+
+			bias := CalculateBias(pop)
+
+			want := 0.5
+
+			if bias != want {
+				t.Errorf("wrong bias, wanted %v, got %v\n", want, bias)
+			}
+		})
+	})
+
+	t.Run("testing with diff length multi-chromosomes", func(t *testing.T) {
+		var d = DNA{
+			Chromosome{1.0},
+			Chromosome{3.0, 3.0},
+			// Chromosome{7.0, 8.0},
+		}
+
+		var d2 = DNA{
+			Chromosome{1.0},
+			Chromosome{3.0, 3.0},
+			// Chromosome{-7.0, 8.0},
+		}
+
+		var dopp = DNA{
+			Chromosome{-1.0},
+			Chromosome{-3.0, -3.0},
+			// Chromosome{-7.0, 8.0},
+		}
+
+		var o = Organism{
+			DNA:     d,
+			Path:    "",
+			Fitness: 0.0,
+		}
+
+		var o2 = Organism{
+			DNA:     d2,
+			Path:    "",
+			Fitness: 0.0,
+		}
+
+		var oopp = Organism{
+			DNA:     dopp,
+			Path:    "",
+			Fitness: 0.0,
+		}
+
+		t.Run("testing exactly the same", func(t *testing.T) {
+			pop := []Organism{o, o2}
+
+			bias := CalculateBias(pop)
+
+			want := 1.0
+
+			if bias != want {
+				t.Errorf("wrong bias, wanted %v, got %v\n", want, bias)
+			}
+		})
+
+		t.Run("testing exactly the opposite", func(t *testing.T) {
+			pop := []Organism{o, oopp}
+
+			bias := CalculateBias(pop)
+
+			want := 0.5
+
+			if bias != want {
+				t.Errorf("wrong bias, wanted %v, got %v\n", want, bias)
+			}
+		})
+	})
+
 }
